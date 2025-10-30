63d2df3eda1df8401d49547f42d16a236f5a4239
LucasCarrasqueiraBartual <lucascarrasqueira@paucasesnovescifp.cat>
2025-10-30 08:39:24 +0100

    routes.dart

---
+++ lib/routes/routes.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_application/screens/HomePage.dart';
+import 'package:flutter_application/screens/alert_page.dart';
+import 'package:flutter_application/screens/avatar_page.dart';
+
+final rutes =  <String, WidgetBuilder>{
+        '/'     : (BuildContext context) => Homepage(),
+        'alert' : (BuildContext context) => AlertPage(),
+        'avatar': (BuildContext context) => AvatarPage()
+};
